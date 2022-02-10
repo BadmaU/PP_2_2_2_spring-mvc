3bc8d4d625a7d95786f90488aaad6486e6cef884
BadmaU <61361339+BadmaU@users.noreply.github.com>
2022-02-10 18:22:33 +0300

    2
    перенес методы из дао в сервис

--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -5,6 +5,7 @@ import web.DAO.CarDAO;
 import web.DAO.CarDaoImpl;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -13,15 +14,27 @@ public class CarServiceImpl implements CarService{
     public CarServiceImpl() {
     }
 
-    CarDAO carDAO = new CarDaoImpl();
+    private List<Car> cars;
+    {
+        cars = new ArrayList<>();
+        cars.add(new Car("car867", 867, "User1"));
+        cars.add(new Car("car394", 394, "User2"));
+        cars.add(new Car("car234", 234, "User3"));
+        cars.add(new Car("car456", 456, "User4"));
+        cars.add(new Car("car568", 568, "User5"));
+    }
 
     @Override
     public List<Car> showAllCar() {
-        return carDAO.showAllCar();
+        return cars;
     }
 
     @Override
     public List<Car> showSomeCar(int count) {
-        return carDAO.showSomeCar(count);
+        if(count > 0 && count < 5) {
+            return cars.stream().limit(count).toList();
+        }else {
+            return cars;
+        }
     }
 }
